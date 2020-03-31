0ad82e84fdb6f33141031fd53b52148d7a4ecb31
shetj <375105540@qq.com>
2020-03-31 11:06:10 +0800

    [F] 防止存在释放的时候出现异常导致崩溃

--- recorder/src/main/java/me/shetj/mixRecorder/AudioDecoder.kt
+++ recorder/src/main/java/me/shetj/mixRecorder/AudioDecoder.kt
@@ -233,11 +233,16 @@ class AudioDecoder {
     }
 
     private fun releaseDecode() {
-        if (mediaDecode != null) {
-            mediaDecode!!.release()
-        }
-        if (mediaExtractor != null) {
-            mediaExtractor!!.release()
+        try {
+            if (mediaDecode != null) {
+                mediaDecode!!.stop();
+                mediaDecode!!.release()
+            }
+            if (mediaExtractor != null) {
+                mediaExtractor!!.release()
+            }
+        } catch (e:Exception) {
+            Log.e("mixRecorder","message = ${e.message}")
         }
     }
 
